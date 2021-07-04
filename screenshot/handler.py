95be03e6906ec23d2484f21c3a7e4288eb852d07
Roger Generoso Masós <roger21gm@gmail.com>
2021-07-04 15:37:17 +0200

    Solved low /dev/shm docker default

--- screenshot/handler.py
+++ screenshot/handler.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    driver = webdriver.Chrome()
+    
+    options = webdriver.ChromeOptions()
+
+    image_width = os.getenv('image_width', '1920')
+    image_height = os.getenv('image_height', '1080')
+    options.add_argument(f"window-size={image_width},{image_height}")
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(chrome_options=options)
     
     driver.get(req)
     screenshot = driver.get_screenshot_as_base64()
